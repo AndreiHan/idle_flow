7cede204adf6e8fbdaf344fd872f24adfbb98210
AndreiHan <hanandrei@gmail.com>
2025-09-30 21:07:08 +0300

    Bump ver + minor changes

--- flow/build.rs
+++ flow/build.rs
@@ -1,7 +1,8 @@
 #![cfg(windows)]
 fn main() {
     tauri_winres::WindowsResource::new()
-        .set_icon_with_id("../resources/icon.ico", "icon-5")
+        .set_icon_with_id("../resources/icon.ico", "icon-1")
+        .set_manifest_file("../resources/manifest.xml")
         .compile()
         .expect("Failed to compile Windows resources");
 }
