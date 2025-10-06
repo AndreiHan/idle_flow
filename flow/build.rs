b3fb1b9e12bc0cabe40fe2138b4da0b94c4fa9cd
AndreiHan <hanandrei@gmail.com>
2025-10-06 21:49:34 +0300

    Fix `side-to-side` config errror

--- flow/build.rs
+++ flow/build.rs
@@ -1,5 +1,8 @@
 #![cfg(windows)]
 fn main() {
+    println!("cargo:rerun-if-changed=../resources/manifest.xml");
+    println!("cargo:rerun-if-changed=../resources/icon.ico");
+    println!("cargo:rerun-if-changed=../resources/icon.png");
     tauri_winres::WindowsResource::new()
         .set_icon_with_id("../resources/icon.ico", "icon-1")
         .set_manifest_file("../resources/manifest.xml")
